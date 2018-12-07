f746b38a6e588f5a78857b557946c9d4e87e9c07
Michael Terranova <newfoundland7@gmail.com>
2018-12-06 21:23:09 -0800

    Initial commit.

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react'
+import RootNavigator from './RootNavigator'
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <RootNavigator/>
+    )
+  }
+}
